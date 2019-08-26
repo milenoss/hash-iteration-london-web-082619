6b854c6dd28a8a48cc4d06e67e183042d90d1ef6
Milen Khanal <milenkhanal@gmail.com>
2019-08-26 18:31:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,6 +11,7 @@ def happy_birthday(birthday_kids)
    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
  else 
    "You are too old for this".
-end
-end
-end
+  end
+  end
+  birtday_kids
+  end
